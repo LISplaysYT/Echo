9f25151106131c31037fc7ea8d405db13967c068
LISplaysYT <85375268+LISplaysYT@users.noreply.github.com>
2023-10-15 17:52:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const BoilerplateClient = require("./src/util/client");
 const ChalkAdvanced = require("chalk-advanced");
+const EmbedBuilder = require("discord.js")
+const Events = require("discord.js")
+const { connect } = require("mongoose");
+require("dotenv").config();
 
 const client = new BoilerplateClient();
 
-client.loginBot().then(() => {
+client.login(process.env.TOKEN);
+
+client.on("ready", async () => {
   console.log(
     `${ChalkAdvanced.white("Boilerplate Bot")} ${ChalkAdvanced.gray(
       ">"
-    )} ${ChalkAdvanced.green("Bot sucessfully started. ")}`
+    )} ${ChalkAdvanced.green("Bot successfully started. ")}`
   );
+  await connect(process.env.databaseToken); {
+    console.log(
+      `${ChalkAdvanced.green("[DATABASE]")} ${ChalkAdvanced.gray(
+        ">"
+      )} ${ChalkAdvanced.white("Successfully connected to MongoDB")}`
+    );
+  }
 });
